da89b5ee5f05b4c934542fb6a1d26072984c7914
Victor Nieves Sanchez <victor.nieves.sanchez@alumnos.upm.es>
2019-11-24 17:09:20 +0100

    añadidos scripts de ejecución

---
+++ scripts/run_almacen.sh
@@ -0,0 +1,8 @@
+# Move to the directory
+cd Almacen
+
+# Clean and package
+mvn clean package
+
+# Run app
+java -jar target/Almacen-1.0-SNAPSHOT-jar-with-dependencies.jar 
---
+++ scripts/run_tienda.sh
@@ -0,0 +1,8 @@
+# Move to the directory
+cd Tienda
+
+# Clean and package
+mvn clean package
+
+# Run app
+java -jar target/Tienda-1.0-SNAPSHOT-jar-with-dependencies.jar
